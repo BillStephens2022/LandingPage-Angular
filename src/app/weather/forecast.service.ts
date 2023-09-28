7e589de1f92af6e60590e319953b8abecbb29625
BillStephens2022 <two4onebill@yahoo.com>
2023-09-28 16:49:58 -0400

    added notifications to forecast service

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,47 +1,63 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map, switchMap, pluck, mergeMap, of, filter, toArray, share } from 'rxjs';
+import {
+  Observable,
+  map,
+  tap,
+  switchMap,
+  pluck,
+  mergeMap,
+  of,
+  filter,
+  toArray,
+  share,
+} from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { NotificationsService } from '../notifications/notifications.service';
 
 interface OpenWeatherResponse {
   list: {
-   dt_txt: string;
-   main: {
-    temp: number;
-   }
-  }[]
+    dt_txt: string;
+    main: {
+      temp: number;
+    };
+  }[];
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class ForecastService {
-  private url = 'https://api.openweathermap.org/data/2.5/forecast'
+  private url = 'https://api.openweathermap.org/data/2.5/forecast';
   private apiKey: string = environment.openWeatherApiKey;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private notificationsService: NotificationsService
+  ) {}
   getForecast() {
-    return this.getCurrentLocation()
-      .pipe(
-        map(coords => {
-          return new HttpParams()
-            .set('lat', String(coords.latitude))
-            .set('lon', String(coords.longitude))
-            .set('units', "imperial")
-            .set('appid', this.apiKey)
-        }),
-        switchMap(params => this.http.get<OpenWeatherResponse>(this.url, { params })),
-        pluck('list'),
-        mergeMap(value => of(...value)),
-        filter((value, index) => index % 8 === 0),
-        map(value => {
-          return {
-            dateString: value.dt_txt,
-            temp: value.main.temp
-          };
-        }),
-        toArray(),
-        share()
-      );
+    return this.getCurrentLocation().pipe(
+      map((coords) => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'imperial')
+          .set('appid', this.apiKey);
+      }),
+      switchMap((params) =>
+        this.http.get<OpenWeatherResponse>(this.url, { params })
+      ),
+      pluck('list'),
+      mergeMap((value) => of(...value)),
+      filter((value, index) => index % 8 === 0),
+      map((value) => {
+        return {
+          dateString: value.dt_txt,
+          temp: value.main.temp,
+        };
+      }),
+      toArray(),
+      share()
+    );
   }
 
   getCurrentLocation() {
@@ -53,6 +69,12 @@ export class ForecastService {
         },
         (err) => observer.error(err)
       );
-    });
+    }).pipe(
+      tap(() => {
+        this.notificationsService.addSuccess('Got your location!');
+      }, () => {
+        this.notificationsService.addError('Failed to get your location!');
+      })
+    );
   }
 }
